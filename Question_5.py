66acc65ee17b6455ce0c7df50b4d0298c7fb7ade
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-12-24 00:16:01 +0530

    Add files via upload

---
+++ Question_5.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array = np.random.random((10, 10))
+
+min_value = array.min()
+max_value = array.max()
+
+print("Minimum value:", min_value)
+print("Maximum value:", max_value)
